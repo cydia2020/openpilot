2dae46b182424a0d0cc59d4ee0c0a25062f1aba7
cydia2020 <12470297+cydia2020@users.noreply.github.com>
2021-05-11 14:09:34 +1000

    interceptor tune and smoothing code

--- selfdrive/car/toyota/carcontroller.py
+++ selfdrive/car/toyota/carcontroller.py
@@ -52,7 +52,7 @@ class CarController():
 
     apply_gas = clip(actuators.gas, 0., 1.)
 
-    if CS.CP.enableGasInterceptor:
+    if (CS.CP.enableGasInterceptor) and (CS.out.vEgo < 3):
       # send only negative accel if interceptor is detected. otherwise, send the regular value
       # +0.06 offset to reduce ABS pump usage when OP is engaged
       apply_accel = 0.06 - actuators.brake
@@ -119,7 +119,7 @@ class CarController():
       else:
         can_sends.append(create_accel_command(self.packer, 0, pcm_cancel_cmd, False, lead))
 
-    if (frame % 2 == 0) and (CS.CP.enableGasInterceptor):
+    if (frame % 2 == 0) and (CS.CP.enableGasInterceptor) and (CS.out.vEgo < 3):
       # send exactly zero if apply_gas is zero. Interceptor will send the max between read value and apply_gas.
       # This prevents unexpected pedal range rescaling
       can_sends.append(create_gas_command(self.packer, apply_gas, frame//2))
